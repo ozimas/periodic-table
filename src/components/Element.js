6054adaf110fa563f57f83e54fe788ec0d744285
Oskar <oskar.maslek@gmail.com>
2020-01-09 13:44:23 +0100

    Change class to className

--- src/components/Element.js
+++ src/components/Element.js
@@ -4,10 +4,10 @@ class Element extends React.Component {
   render() {
     return (
       <div className="element">
-        <div class="element__id">{this.props.id}</div>
-        <div class="element__sym">{this.props.symbol}</div>
-        <div class="element__name">{this.props.name}</div>
-        <div class="element__atomic-mass">{this.props.atomicMass}</div>
+        <div className="element__id">{this.props.id}</div>
+        <div className="element__sym">{this.props.symbol}</div>
+        <div className="element__name">{this.props.name}</div>
+        <div className="element__atomic-mass">{this.props.atomicMass}</div>
       </div>
     );
   }
